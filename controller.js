77c36ebc2a0c5ca14de2c7af9bcc97da0983d041
freddy <freddy@fktech.com>
2021-09-02 12:15:37 -0300

    agrego archivos a mi repositorio

---
+++ controller.js
@@ -0,0 +1,12 @@
+let input = window.prompt("ingrese dato: ");
+
+if( !isNaN(input) ){
+    window.alert("esto es un numero")
+}else{
+    window.alert("esto NO es un numero")
+}abc123
+
+let nombre = window.prompt( 'ingrese contraseña:' )
+
+if( nombre === 'Abc123') window.alert( 'Contraseña incorrecta' )
+else window.alert( 'Contraseña incorrecta' )
