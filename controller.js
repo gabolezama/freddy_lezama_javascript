28e12a68e043d32aa9f538f8e8c3be68bc731d42
freddy <freddy@fktech.com>
2021-09-02 12:47:39 -0300

    Desafios entregables 1 y 2 curso de JS

--- controller.js
+++ controller.js
@@ -1,12 +1,54 @@
 let input = window.prompt("ingrese dato: ");
 
-if( !isNaN(input) ){
-    window.alert("esto es un numero")
-}else{
-    window.alert("esto NO es un numero")
-}abc123
+if (!isNaN(input)) {
+    if(input.trim() ==='' || input === undefined ) input = 0
 
-let nombre = window.prompt( 'ingrese contraseña:' )
+    window.alert("esto es un numero");
 
-if( nombre === 'Abc123') window.alert( 'Contraseña incorrecta' )
-else window.alert( 'Contraseña incorrecta' )
+    let segundoOperando = 0
+    let operacion = window.prompt("Multiplicar 'x' -- Sumar '+' -- Restar '-' -- Dividir '/'")
+    let signoValido = (operacion.trim() === '/' || operacion.trim() === '+' || operacion.trim() === '-' || operacion.trim() === 'x')
+
+    if (signoValido) {
+        input = parseInt(input);
+
+        segundoOperando = window.prompt("ingrese segundo operando: ")
+        
+        if (!isNaN(segundoOperando)) {
+            let resultado = 0
+            segundoOperando = parseInt(segundoOperando)
+
+            if (operacion === '/') {
+                resultado = input / segundoOperando
+            } else if (operacion === '+') {
+                resultado = input + segundoOperando
+            } else if (operacion === '-') {
+                resultado = input - segundoOperando
+            } else if (operacion === 'x') {
+                resultado = input * segundoOperando
+            } else {
+                window.alert("La operacion NO es invalida, resultado = 0")
+                resultado = 0
+            }
+
+            window.alert(`EL resultado de la operacion ${input} ${operacion} ${segundoOperando} es: ${resultado} `)
+            
+        } else {
+            window.alert("Segundo Operando NO es un numero, fin del programa")
+        }
+    } else {
+        window.alert("La operacion NO es invalida, fin del programa")
+    }
+} else {
+    window.alert("esto NO es un numero, fin del programa");
+}
+
+// let contraseña = window.prompt( 'ingrese contraseña:' );
+// let isValidPassword = ( contraseña === 'Abc123' );
+
+// if( isValidPassword ){ 
+//     window.alert( 'Contraseña correcta' )
+// }
+// else{
+//      window.alert( 'Contraseña incorrecta' )
+//     }
